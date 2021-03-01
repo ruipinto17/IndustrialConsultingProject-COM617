add4a124f2eeec336ef3ef360aeb2e3359edda87
tebogo dube <tsuper612@gmail.com>
2021-03-01 05:27:35 +0000

    Documatation of linking software

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/*Paste the URL from any of components on grafana dashboard (tick off the current timeframe before embedding)*/}
-        <iframe src="http://localhost:3000/d-solo/-WL7JUPGz/apache-jmeter-dashboard-using-core-influxdbbackendlistenerclient?orgId=1&refresh=5s&var-data_source=InfluxDB&var-application=application%20name&var-transaction=&var-measurement_name=jmeter&var-send_interval=5&panelId=4" width="450" height="200" frameborder="0"></iframe>
+        <iframe
+          src="http://localhost:3000/d/gwHyaUyMk/apache-jmeter-test-1?orgId=1&var-data_source=InfluxDB&var-application=application%20name&var-transaction=HTTP%20Request&var-measurement_name=jmeter&var-send_interval=5"
+          width="450"
+          height="200"
+          frameborder="0"
+        ></iframe>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
