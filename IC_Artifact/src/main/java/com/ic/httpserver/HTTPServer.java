7d241bdb11b3b107bdd8bd0dc65dc7bbee8bd95d
tebogo dube <tsuper612@gmail.com>
2021-04-08 14:16:46 +0100

    retriving data from javascript project

--- IC_Artifact/src/main/java/com/ic/httpserver/HTTPServer.java
+++ IC_Artifact/src/main/java/com/ic/httpserver/HTTPServer.java
@@ -5,7 +5,9 @@
  */
 package com.ic.httpserver;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -25,68 +27,49 @@ import java.util.Map;
  * @author leite
  */
 public class HTTPServer {
-    
-    
-    public static void main(String args[]) {
+
+    public static void main(String args[]) throws IOException {
         try {
-            
+
             System.out.print("Starting HTTP Server\n");
-            
+
             String[] parameters = new String[30];
-            
+
             // Create a ServerSocket to listen on that port.
-            ServerSocket ss = new ServerSocket(5009);
+            ServerSocket ss = new ServerSocket(5008);
 
             // Loop for handling connections
             System.out.print("Loop Starting\n");
-            for (;;) {
-                
 
-                Socket client = ss.accept();
-                System.out.print("Connections Established\n");
+            Socket client = ss.accept();
+            System.out.print("Connections Established\n");
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                PrintWriter out = new PrintWriter(client.getOutputStream());
+            DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
+            PrintWriter out = new PrintWriter(client.getOutputStream());
 
-                out.print("HTTP/1.1 200 \r\n"); 
-                out.print("Content-Type: text/plain\r\n");
-                out.print("Connection: close\r\n");
-                out.print("\r\n"); 
-                
-                System.out.print("Extracting Parameters\n");
-                int i = 0;
-                String line;
-                while ((line = in.readLine()) != null) {
-                    if (line.length() == 0)
-                        break;
-//                    out.print(line + "\r\n");
-                    parameters[i] = line;
-//                    System.out.println(i + " - " + parameters[i]);
-                    i++;
+            out.print("HTTP/1.1 200 \r\n");
+            out.print("Content-Type: text/plain\r\n");
+            out.print("Connection: close\r\n");
+            out.print("\r\n");
+
+            System.out.print("Extracting Parameters\n");
+            int i = 0;
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.toLowerCase().indexOf("thread") != -1) {
+                    String[] linesplit = line.split(" ");
+                    String thread = linesplit[1];
+                    System.out.println(thread);
+                } else if (line.toLowerCase().indexOf("url") != -1) {
+                    String[] linesplit = line.split(" ");
+                    String url = linesplit[1];
+                    System.out.println(url);
                 }
-
-
-                System.out.print("Closing Services\n");
-                out.close(); 
-                in.close(); 
-                client.close(); 
-                System.out.print("Services Closed\n");
-                
-                System.out.print("Extracted Parameters : \n");
-                //Set parameters to variables.                
-                String targetURL = parameters[17].substring(parameters[17].indexOf(":") + 1);               
-                System.out.println("TARGET URL --- " + targetURL);
-                String users = parameters[18].substring(parameters[18].indexOf(":") + 1);               
-                System.out.println("USERS --- " + users);
-                String threads = parameters[19].substring(parameters[19].indexOf(":") + 1);               
-                System.out.println("THREADS --- " + threads);
-                
-            } // Now loop again, waiting for the next connection
-        } 
-        catch (IOException e) {
+                // Now loop again, waiting for the next connection
+            }
+        } catch (IOException e) {
             System.err.println(e);
             System.err.println("Usage: java HttpMirror 5009");
         }
     }
-    
 }
