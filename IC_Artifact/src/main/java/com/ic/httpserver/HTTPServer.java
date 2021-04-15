315c0c1ff174143395a375bd7ee1f138290723be
leite <leitesdzn@gmail.com>
2021-04-15 15:18:45 +0100

    Server is now always listening for request

--- IC_Artifact/src/main/java/com/ic/httpserver/HTTPServer.java
+++ IC_Artifact/src/main/java/com/ic/httpserver/HTTPServer.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -27,71 +28,37 @@ public class HTTPServer {
     public static String threads = "10";
     public static String rampup = "5";
     public static String users = null;
-
+    public static PerformanceTest task = null;
+    public static ScheduledExecutorService exec = null;    
+    public static boolean running = true;
+    public static Socket client = null;
+    public static ServerSocket ss = null;
+    
     public static void main(String args[]) throws IOException {
         try {
-            Socket client;
-
             System.out.print("Starting HTTP Server\n");
 
             String[] parameters = new String[30];
 
             // Create a ServerSocket to listen on that port.
-            ServerSocket ss = new ServerSocket(5008);
-
-            // Loop for handling connections
-            System.out.print("Loop Starting\n");
-
-            client = ss.accept();
-            System.out.print("Connections Established\n");
-
-            DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
-            PrintWriter out = new PrintWriter(client.getOutputStream());
-
-            out.print("HTTP/1.1 200 \r\n");
-            out.print("Content-Type: text/plain\r\n");
-            out.print("Connection: close\r\n");
-            out.print("\r\n");
-
-            System.out.print("Extracting Parameters\n");
-            String line;
-
-            while ((line = in.readLine()) != null) {
-                if (line.toLowerCase().indexOf("targeturl") != -1) {
-                    String[] linesplit = line.split(" ");
-                    targetUrl = linesplit[1];
-                    System.out.println("TargetUrl - " + targetUrl);
-                } else if (line.toLowerCase().indexOf("threads") != -1) {
-                    String[] linesplit = line.split(" ");
-                    threads = linesplit[1];
-                    System.out.println("Threads - " + threads);
-                } else if (line.toLowerCase().indexOf("rampup") != -1) {
-                    String[] linesplit = line.split(" ");
-                    rampup = linesplit[1];
-                    System.out.println("Rampup - " + rampup);
-                } else if (line.toLowerCase().indexOf("users") != -1) {
-                    String[] linesplit = line.split(" ");
-                    users = linesplit[1];
-                    System.out.println("Users - " + users);
-                } else if (line.isEmpty()) {
-                    break;
-                }
-            }
-            ss.close();
-
-            if (ss.isClosed()) {
-                run();
-            }
+            ss = new ServerSocket(5008);
+            while(running == true){
+                System.out.print("Loop Starting\n");
+                client = ss.accept();
+                receiveRequest();
+            }            
 
         } catch (IOException e) {
             System.err.println(e);
             System.err.println("Usage: java HttpMirror 5008");
+        } finally {
+            ss.close(); 
         }
     }
 
     public static void run() {
         try {
-            ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+            exec = Executors.newScheduledThreadPool(1);
 
 //            String PATHTOTESTPAGE = "https://blazedemo.com/reserve.php";
             String METHOD = "POST";
@@ -99,11 +66,59 @@ public class HTTPServer {
 //            String Thread = "4";
 //            String Rampup = "30";
 
-            PerformanceTest task = new PerformanceTest(targetUrl, threads, rampup);
-            exec.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+            task = new PerformanceTest(targetUrl, threads, rampup);
+            exec.scheduleAtFixedRate(task, 0, 1, TimeUnit.MINUTES);
+
         } catch (Exception e) {
             System.out.println(e);
         }
 
+    }
+
+    public static void receiveRequest() throws IOException {
+        System.out.print("Connections Established\n");
+
+        DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+
+        out.print("HTTP/1.1 200 \r\n");
+        out.print("Content-Type: text/plain\r\n");
+        out.print("Connection: close\r\n");
+        out.print("\r\n");
+
+        System.out.print("Extracting Parameters\n");
+        String line;
+
+        while ((line = in.readLine()) != null) {
+            if (line.toLowerCase().indexOf("targeturl") != -1) {
+                String[] linesplit = line.split(" ");
+                targetUrl = linesplit[1];
+                System.out.println("TargetUrl - " + targetUrl);
+            } else if (line.toLowerCase().indexOf("threads") != -1) {
+                String[] linesplit = line.split(" ");
+                threads = linesplit[1];
+                System.out.println("Threads - " + threads);
+            } else if (line.toLowerCase().indexOf("rampup") != -1) {
+                String[] linesplit = line.split(" ");
+                rampup = linesplit[1];
+                System.out.println("Rampup - " + rampup);
+            } else if (line.toLowerCase().indexOf("users") != -1) {
+                String[] linesplit = line.split(" ");
+                users = linesplit[1];
+                System.out.println("Users - " + users);
+            } else if (line.isEmpty()) {
+                break;
+            }
+        }        
+//        ss.close(); 
+//        if (task == null && ss.isClosed()) {
+//            run();
+//        } else {
+            if (exec != null){
+                exec.shutdown();
+            }
+            run();
+//        }
+               
     }
 }
